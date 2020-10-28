b83bd2a2e0c554344a4448973db16cfbbf84f429
Tyler Yep <tyep@cs.stanford.edu>
2020-10-28 11:18:47 -0700

    convert setup.py to metadata

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md") as f:
-    LONG_DESCRIPTION = f.read()
-
-setuptools.setup(
-    name="edutorch",
-    version="0.0.3",
-    author="Tyler Yep",
-    author_email="tyep@cs.stanford.edu",
-    description="Rewritten PyTorch framework designed to help you learn AI/ML",
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type="text/markdown",
-    url="https://github.com/tyleryep/edutorch",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.7",
-)
+setup()
